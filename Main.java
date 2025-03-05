812f4af0483f8a6bc184d91158c2449dc45b77af
parth bhutada <parthbhutada13@gmail.com>
2025-03-05 20:31:09 +0530

    Shuffling the deck and printing new Deck set

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-//Main.java
 import java.util.*;
 
+class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
 
-class Main{
-	public static void main(String[] args){
-		Deck deck = new Deck();
+        deck.shuffleDeck();
+        System.out.println("Shuffled Deck:");
+        deck.printDeck();
+    }
+}
