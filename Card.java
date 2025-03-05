edd90311e76b19d3f8beb9f1c403b8e360fa2a58
parth bhutada <parthbhutada13@gmail.com>
2025-03-05 20:14:09 +0530

    Completed Card.java

--- Card.java
+++ Card.java
@@ -3,10 +3,12 @@
 class Card{
 	String rank;
 	String suit;
-public Card(String rank, String suit){
+	
+	public Card(String rank, String suit){
 		this.rank = rank;
 		this.suit = suit;
-        }
+	}
+
 	public String toString(){
 		return rank + " of " + suit;
 	}
