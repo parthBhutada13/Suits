1c3d7220f1a5e44b10eca4dbf74bb33f759c1ce8
parth bhutada <parthbhutada13@gmail.com>
2025-03-05 20:20:59 +0530

    Defining the card values for suits and ranks and adding them to the arrays

--- Deck.java
+++ Deck.java
@@ -7,3 +7,14 @@ class Deck {
 		deck = new ArrayList<>();
 		createDeck(); 
 	}
+
+	public void createDeck() {
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+
+		for (String rank : ranks) {
+			for (String suit : suits) {
+				deck.add(new Card(rank, suit));
+			}
+		}
+	}
